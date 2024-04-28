3b43fe319f2f92c2c76e48753d7d6c8024926711
Jhonatan Villamizar <jhonatan.avj@gmail.com>
2024-04-28 00:19:12 -0500

    component Image

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,12 +1,21 @@
+import Image from 'next/image';
 import styles from './Description.module.sass';
+
 
 export const Description = () => {
   return (
     <section className={styles.Description}>
-      <img src="/images/description.jpeg" alt="products marketplace" />
+      <Image 
+        src="/images/description.jpeg" 
+        alt="products marketplace" 
+        width={500} 
+        height={300}
+        priority={false}
+        quality={100}
+      />
       <div className={styles.Description__text}>
         <h2>Bring the future today</h2>
-        <p>Future World: Your Gateway to Tomorrow's Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
+        <p>Future World: Your Gateway to Tomorrows Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
       </div>
     </section>
   )
